a4f4586b14f26979196dddb35ec68e7aef65d2ca
ssghaier123 <135118527+ssghaier123@users.noreply.github.com>
2023-06-07 13:08:45 +0100

    fix multer

--- src/helpers/upload.js
+++ src/helpers/upload.js
@@ -1,42 +1,25 @@
+
 const multer = require('multer');
 const fs = require('fs');
-const maxFileSize = 5242880;
-const maxContentLength = maxFileSize + 100; // Add a buffer to the max file size
-
 const storage = multer.diskStorage({
-	destination(req, file, cb) {
-		const path = `files/uploads/${req.body.session}/`;
-		fs.mkdirSync(path, { recursive: true });
-		cb(null, path);
-	},
-	limits: {
-		fileSize: maxFileSize
-	},
-	filename(req, file, cb) {
-		const date = Date.now();
-		cb(null, `${req.body.session}-${date}.${file.mimetype.split('/')[1]}`);
-		req.body.filename = `${req.body.session}-${date}.${file.mimetype.split('/')[1]}`;
-		req.body.filepath = `files/uploads/${req.body.session}/`;
-	}
+    destination(req, file, cb) {
+        const path = `files/uploads/${req.body.session}/`;
+        fs.mkdirSync(path, { recursive: true });
+        cb(null, path);
+    },
+    limits : {
+      fileSize: 524288
+    },
+    filename(req, file, cb) {
+        const date = Date.now();
+        cb(null, `${req.body.session}-${date}.${file.mimetype.split('/')[1]}`);
+        req.body.filename = `${req.body.session}-${date}.${file.mimetype.split('/')[1]}`
+        req.body.filepath = `files/uploads/${req.body.session}/`
+    },
 });
-
 const upload = multer({
-	storage,
-	limits: {
-		fileSize: maxFileSize
-	},
-	fileFilter(req, file, cb) {
-		try {
-			if (file.size <= maxFileSize) {
-				cb(null, true);
-			} else {
-				throw new Error('File size exceeds the limit.');
-			}
-		} catch (error) {
-			console.log('Error:', error);
-			cb(error);
-		}
-	}
+    storage, limits: {
+        fileSize: 524288
+    }
 });
-
 module.exports = { upload };
