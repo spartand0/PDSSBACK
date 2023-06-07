0ebe5195e09881b9588d40c49e739c63093bb3cc
ssghaier123 <135118527+ssghaier123@users.noreply.github.com>
2023-06-07 12:38:16 +0100

    fix multer

--- src/helpers/upload.js
+++ src/helpers/upload.js
@@ -2,36 +2,38 @@ const multer = require('multer');
 const fs = require('fs');
 const maxFileSize = 5242880;
 const maxContentLength = maxFileSize + 100; // Add a buffer to the max file size
+
 const storage = multer.diskStorage({
-	destination(req, file, cb) {
-		const path = `files/uploads/${req.body.session}/`;
-		fs.mkdirSync(path, { recursive: true });
-		cb(null, path);
-	},
-	filename(req, file, cb) {
-		const date = Date.now();
-		cb(null, `${req.body.session}-${date}.${file.mimetype.split('/')[1]}`);
-		req.body.filename = `${req.body.session}-${date}.${file.mimetype.split('/')[1]}`;
-		req.body.filepath = `files/uploads/${req.body.session}/`;
-	}
+  destination(req, file, cb) {
+    const path = `files/uploads/${req.body.session}/`;
+    fs.mkdirSync(path, { recursive: true });
+    cb(null, path);
+  },
+  filename(req, file, cb) {
+    const date = Date.now();
+    cb(null, `${req.body.session}-${date}.${file.mimetype.split('/')[1]}`);
+    req.body.filename = `${req.body.session}-${date}.${file.mimetype.split('/')[1]}`;
+    req.body.filepath = `files/uploads/${req.body.session}/`;
+  }
 });
+
 const upload = multer({
-	storage,
-	limits: {
-		fileSize: maxFileSize,
-		contentLength: maxContentLength // Add content length limit
-	},
-	fileFilter(req, file, cb) {
-		const fileSize = parseInt(req.headers['content-length']);
-		try {
-			if (fileSize <= maxFileSize) {
-				cb(null, true);
-			} else {
-				throw 'File size exceeds the limit.';
-			}
-		} catch (error) {
-			console.log('error', error);
-		}
-	}
+  storage,
+  limits: {
+    fileSize: maxFileSize
+  },
+  fileFilter(req, file, cb) {
+    try {
+      if (file.size <= maxFileSize) {
+        cb(null, true);
+      } else {
+        throw new Error('File size exceeds the limit.');
+      }
+    } catch (error) {
+      console.log('Error:', error);
+      cb(error);
+    }
+  }
 });
+
 module.exports = { upload };
